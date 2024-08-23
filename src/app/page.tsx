981bea57cdb48300a33912f2c575cbec89409ef6
DebabrataKarfa2022s <debabratakarfa2002@gmail.com>
2024-08-23 20:24:05 +0530

    add padding in footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
       </main>
 
       {/* Footer */}
-      <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
+      <footer className="text-center p-6 md:p-6 bg-gray-900 text-white">
         &copy;{new Date().getFullYear()} True Feedback. All rights reserved.
       </footer>
     </>
